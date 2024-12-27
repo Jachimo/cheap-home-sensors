e774a563ad862da07488578bd53c9a4f62000e9f
Jachimo <Jachimo@users.noreply.github.com>
2024-12-27 14:34:52 -0500

    Put back in commented lines for BME sensor in this version; not tested

--- TempSensor/main.py
+++ TempSensor/main.py
@@ -73,10 +73,11 @@ async def main():
         wifi_t = asyncio.create_task(wifi_manager.monitor_connection())
         ntp_t = asyncio.create_task(set_rtc_ntp(wifi_manager))
         
+        #bme_t = asyncio.create_task(acquire_transmit(wifi_manager, sensor_bme, mqtt_manager))
         dht_t = asyncio.create_task(acquire_transmit(wifi_manager, sensor_dht, mqtt_manager))
         ds_t  = asyncio.create_task(acquire_transmit(wifi_manager, sensor_ds, mqtt_manager))
         
-        await asyncio.gather(wifi_t, ntp_t, dht_t)
+        await asyncio.gather(wifi_t, ntp_t, dht_t) #, bme_t)
 
     except Exception as e:
         print("Exception - terminating")
@@ -85,6 +86,8 @@ async def main():
             wifi_t.cancel()
             ntp_t.cancel()
             dht_t.cancel()
+            ds_t.cancel()
+            #bme_t.cancel()
         except (asyncio.CancelledError, NameError):
             print("Tasks cancelled")
     
